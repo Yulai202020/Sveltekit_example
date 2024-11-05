ba92c9e8c19456cb0b53ffd26aa65e775c37f896
Yulai202020 <ngmyulai@gmail.com>
2024-11-05 11:01:59 +0400

    first comit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     <div class="container">
         <a href="/ifs">If statement example</a>
         <a href="/for">For statement example</a>
-        <a href="/ifs">imports example</a>
+        <a href="/imports">imports example</a>
         <a href="/snippet">snippet example</a>
     <div>
 </main>
